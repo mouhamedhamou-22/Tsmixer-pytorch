f55ea07eac36a0268028e21be2e0b5ba5771edbd
mouhamedhamou-22 <computerstudies07@gmail.com>
2024-06-02 00:48:44 +0100

    FF

--- models/TSMixer.py
+++ models/TSMixer.py
@@ -131,16 +131,113 @@ class Backbone(nn.Module):
         self.pred_len = pred_len = configs.pred_len
         self.enc_in = enc_in = configs.enc_in
         self.layer_num = layer_num = 1
-         
+
         self.mix_layer = Mixer_Layer(seq_len, enc_in)
         self.temp_proj = nn.Linear(self.seq_len, self.pred_len)
-
-    def forward(self, x):# B, L, D -> B, H, D
+        # Define a convolutional layer
+        self.conv_layer = nn.Conv1d(in_channels=self.enc_in, out_channels=self.enc_in, kernel_size=3, padding=1)
+    def forward(self, x): # B, L, D -> B, H, D
+        # Apply convolutional layer
+        # x = x.permute(0, 2, 1)  # B, L, D -> B, D, L
+        # x = self.conv_layer(x)  # B, D, L -> B, D, L
+        # x = x.permute(0, 2, 1)  # B, D, L -> B, L, D
         n_block = 6
         for _ in range(n_block):
-            x = self.mix_layer(x)   
+           x = self.mix_layer(x)# B, L, D -> B, L, D
         x = self.temp_proj(x.permute(0, 2, 1)).permute(0, 2, 1) # B, L, D -> B, H, D
         return x
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = nn.GELU()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+        
+class Backbone_cov(nn.Module):
+    def __init__(self, configs):
+        super(Backbone_cov, self).__init__()
+
+        self.seq_len = seq_len = configs.seq_len
+        self.pred_len = pred_len = configs.pred_len
+
+        # Patching
+        self.patch_len = patch_len = 16 # 16
+        self.stride = stride = 8  # 8
+        self.patch_num = patch_num = int((seq_len - patch_len) / stride + 1)
+        self.padding_patch = "end"
+        if self.padding_patch == 'end':  # can be modified to general case
+            self.padding_patch_layer = nn.ReplicationPad1d((0, stride))
+            self.patch_num = patch_num = patch_num + 1
+
+        # 1
+        d_model = patch_len * patch_len
+        self.embed = nn.Linear(patch_len, d_model)
+        self.dropout_embed = nn.Dropout(0.3)
+
+        # 2
+        # self.lin_res = nn.Linear(seq_len, pred_len) # direct res, seems bad
+        self.lin_res = nn.Linear(patch_num * d_model, pred_len)
+        self.dropout_res = nn.Dropout(0.3)
+
+        # 3.1
+        self.depth_conv = nn.Conv1d(patch_num, patch_num, kernel_size=patch_len, stride=patch_len, groups=patch_num)
+        self.depth_activation = nn.GELU()
+        self.depth_norm = nn.BatchNorm1d(patch_num)
+        self.depth_res = nn.Linear(d_model, patch_len)
+        # 3.2
+        # self.point_conv = nn.Conv1d(patch_len,patch_len,kernel_size=1, stride=1)
+        # self.point_activation = nn.GELU()
+        # self.point_norm = nn.BatchNorm1d(patch_len)
+        self.point_conv = nn.Conv1d(patch_num, patch_num, kernel_size=1, stride=1)
+        self.point_activation = nn.GELU()
+        self.point_norm = nn.BatchNorm1d(patch_num)
+        # 4
+        self.mlp = Mlp(patch_len * patch_num, pred_len * 2, pred_len)
+
+    def forward(self, x): # B, L, D -> B, H, D
+        B, _, D = x.shape
+        L = self.patch_num
+        P = self.patch_len
+
+        # z_res = self.lin_res(x.permute(0, 2, 1)) # B, L, D -> B, H, D
+        # z_res = self.dropout_res(z_res)
+
+        # 1
+        if self.padding_patch == 'end':
+            z = self.padding_patch_layer(x.permute(0, 2, 1))  # B, L, D -> B, D, L -> B, D, L
+        z = z.unfold(dimension=-1, size=self.patch_len, step=self.stride) # B, D, L, P
+        z = z.reshape(B * D, L, P, 1).squeeze(-1)
+        z = self.embed(z) # B * D, L, P -> # B * D, L, d
+        z = self.dropout_embed(z)
+
+        # 3.1
+        res = self.depth_res(z) # B * D, L, d -> B * D, L, P
+        z_depth = self.depth_conv(z) # B * D, L, d -> B * D, L, P
+        z_depth = self.depth_activation(z_depth)
+        z_depth = self.depth_norm(z_depth)
+        z_depth = z_depth + res
+        # 3.2
+        z_point = self.point_conv(z_depth) # B * D, L, P -> B * D, L, P
+        z_point = self.point_activation(z_point)
+        z_point = self.point_norm(z_point)
+        z_point = z_point.reshape(B, D, -1) # B * D, L, P -> B, D, L * P
+
+        # 4
+        z_mlp = self.mlp(z_point) # B, D, L * P -> B, D, H
+
+        return (z_mlp).permute(0,2,1)
 
 class Model(nn.Module):
 
@@ -149,14 +246,15 @@ class Model(nn.Module):
         self.rev = RevIN(configs.enc_in)
 
         self.backbone = Backbone(configs)
+        self.Backbone_cov = Backbone_cov(configs)
 
         self.seq_len = configs.seq_len
         self.pred_len = configs.pred_len
 
 
     def forward(self, x, batch_x_mark, dec_inp, batch_y_mark):
-        # z = self.rev(x, 'norm') # B, L, D -> B, L, D
-        z = self.backbone(x) # B, L, D -> B, H, D
-        # z = self.rev(z, 'denorm') # B, H, D -> B, H, D
+        z = self.rev(x, 'norm') # B, L, D -> B, L, D
+        z = self.backbone(z) # B, L, D -> B, H, D
+        #z = self.Backbone_cov(z)
+        z = self.rev(z, 'denorm') # B, H, D -> B, H, D
         return z
-
