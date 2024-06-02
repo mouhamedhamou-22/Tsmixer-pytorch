a6cdc9098662281966248ded7b7ced0fca4e8f7a
mouhamedhamou-22 <computerstudies07@gmail.com>
2024-06-02 18:01:38 +0100

    FF

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -299,7 +299,7 @@ class Exp_Main(Exp_Basic):
                     input = batch_x.detach().cpu().numpy()
                     gt = np.concatenate((input[0, :, -1], true[0, :, -1]), axis=0)
                     pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
-                    visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'), data_name=setting, seq_len=batch_x.shape[1], pred_len=self.args.pred_len)
+                    visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
 
         if self.args.test_flop:
             test_params_flop((batch_x.shape[1],batch_x.shape[2]))
